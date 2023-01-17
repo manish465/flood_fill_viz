a7e1bfad2022daf24ef0fbabf27447b6563108cf
manish <46858945+manish465@users.noreply.github.com>
2023-01-17 18:13:14 +0530

    project init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import { useState } from "react";
-import Board from "./components/Board";
-import Header from "./components/Header";
-import "./styles.scss";
-
 const App = () => {
-    const [rows, setRows] = useState(10);
-    const [cols, setCols] = useState(10);
-    const [wallMode, setWallMode] = useState(true);
-
-    return (
-        <div className="app">
-            <Header
-                rows={rows}
-                cols={cols}
-                setRows={setRows}
-                setCols={setCols}
-                wallMode={wallMode}
-                setWallMode={setWallMode}
-            />
-            <Board
-                rows={rows}
-                cols={cols}
-                wallMode={wallMode}
-                setWallMode={setWallMode}
-            />
-        </div>
-    );
+    return <h1>Hello World</h1>;
 };
 
 export default App;
