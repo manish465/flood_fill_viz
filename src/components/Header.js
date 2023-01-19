20c18decab6cd9349646ee4ba142f3b8f790f953
manish <46858945+manish465@users.noreply.github.com>
2023-01-19 20:07:07 +0530

    Added styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,22 +14,28 @@ const Header = ({
             >
                 FLOOD FILL
             </h1>
-            <input
-                disabled={cellStatus.checked}
-                type="number"
-                name="rows"
-                id="rows"
-                value={boardDimension.rows}
-                onChange={(e) => handleRows(e)}
-            />
-            <input
-                disabled={cellStatus.checked}
-                type="number"
-                name="coloumns"
-                id="coloumns"
-                value={boardDimension.coloumns}
-                onChange={(e) => handleColoumns(e)}
-            />
+            <form>
+                <label htmlFor="rows">Rows : </label>
+                <input
+                    disabled={cellStatus.checked}
+                    type="number"
+                    name="rows"
+                    id="rows"
+                    value={boardDimension.rows}
+                    onChange={(e) => handleRows(e)}
+                />
+            </form>
+            <form>
+                <label htmlFor="coloumns">Coloumns : </label>
+                <input
+                    disabled={cellStatus.checked}
+                    type="number"
+                    name="coloumns"
+                    id="coloumns"
+                    value={boardDimension.coloumns}
+                    onChange={(e) => handleColoumns(e)}
+                />
+            </form>
             <button
                 disabled={cellStatus.isFillMode}
                 onClick={() =>
